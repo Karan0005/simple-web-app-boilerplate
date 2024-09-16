d6ce178658d47a4142404d45c7f0235724cbe05d
Karan Gupta <admin@joborce.com>
2024-09-16 11:59:12 +0530

    fix test and build

--- apps/frontend/src/modules/app.component.spec.ts
+++ apps/frontend/src/modules/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { CONFIG } from '../config/config';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,7 +11,12 @@ describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AppComponent],
-            imports: [AppRoutingModule]
+            imports: [
+                AppRoutingModule,
+                LoggerModule.forRoot({
+                    level: CONFIG.logLevel === 'debug' ? NgxLoggerLevel.DEBUG : NgxLoggerLevel.ERROR
+                })
+            ]
         }).compileComponents();
     });
 
