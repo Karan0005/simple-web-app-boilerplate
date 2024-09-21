9f30658313dde85236c1a91943f7e564dfd6ff1e
Karan Gupta <karangupta0005@gmail.com>
2024-09-21 16:59:21 +0530

    test

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-//testing
+//testing...
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './modules/app.module';
 
