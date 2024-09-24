803dbc4d13cc66064f1b6fa36c28bcff5df54de9
Karan Gupta <karangupta0005@gmail.com>
2024-09-24 14:56:47 +0530

    fix

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,6 +1,12 @@
 /* eslint-disable no-console */
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CONFIG } from './config/config';
 import { AppModule } from './modules/app.module';
+
+if (CONFIG.production) {
+    enableProdMode();
+}
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
