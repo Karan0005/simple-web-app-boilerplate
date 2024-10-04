44a7289fff5046db77048965787a798b86c86993
Karan Gupta <karangupta0005@gmail.com>
2024-10-04 12:23:46 +0530

    Fix comments

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -19,24 +19,28 @@ import { LoggerTransports, setupSecretValues } from './config';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
-    //Setting Up Logger
+    // Initialize a Winston logger for the application
     const appLogger = WinstonModule.createLogger({
         format: winston.format.uncolorize(),
         transports: LoggerTransports
     });
 
-    //Setting Up Environment Variables
+    // Set up environment variables (e.g., API keys or sensitive values)
     await setupSecretValues(appLogger);
+
+    // Create a new NestJS Express application
     const app = await NestFactory.create<NestExpressApplication>(AppModule, {
         bodyParser: true,
         rawBody: true
     });
+
+    // Fetch configuration values for routes and server settings
     const configService = app.get(ConfigService);
     const routePrefix = configService.get('server.routePrefix') || 'api';
     const serverSecret: string = configService.get('server.secret') as string;
     const apiBaseURL: string = configService.get('server.apiBaseURL') as string;
 
-    // Setting Up CORS Policy Security
+    // Enable CORS with allowed origins, methods, and headers
     app.enableCors({
         origin: configService.get('server.appBaseURL'),
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
@@ -47,14 +51,20 @@ async function bootstrap() {
         credentials: true
     });
 
-    // Setting Up URL Policy Security
+    // Allow URL-encoded data parsing for incoming requests
     app.use(express.urlencoded({ extended: true }));
 
-    //Setting Up Middleware
+    // Apply global exception filter and response interceptor
     app.useGlobalFilters(new ExceptionProcessor(appLogger));
     app.useGlobalInterceptors(new ResponseProcessor());
+
+    // Use custom logger for logging across the app
     app.useLogger(appLogger);
+
+    // Set a global route prefix (e.g., '/api')
     app.setGlobalPrefix(routePrefix);
+
+    // Apply global validation with transformation and strict whitelisting
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
@@ -63,7 +73,7 @@ async function bootstrap() {
         })
     );
 
-    //Setting Up Swagger Documentation
+    // Set up basic authentication for accessing Swagger documentation
     app.use(
         ['/' + routePrefix + '/swagger', '/' + routePrefix + '/swagger-json'],
         basicAuth({
@@ -74,6 +84,7 @@ async function bootstrap() {
         })
     );
 
+    // Configure Swagger documentation settings
     const options = new DocumentBuilder()
         .setTitle('Backend App')
         .setDescription('API Documentation')
@@ -81,20 +92,12 @@ async function bootstrap() {
         .addBearerAuth()
         .addServer(apiBaseURL)
         .build();
+
+    // Generate the Swagger document and set up the endpoint
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup(routePrefix + '/swagger', app, document);
 
-    // Setting Up Application Helmet Policy Security
-    /**
-     * This header helps prevent cross-site scripting (XSS) attacks by restricting the resources that can be loaded by the application
-     * Helps prevent click jacking attacks by restricting the application from being embedded in a frame
-     * This header helps prevent attackers from learning the technology stack used by the application
-     * This header helps prevent man-in-the-middle (MITM) attacks by enforcing the use of HTTPS
-     * This header helps prevent attackers from exploiting MIME type sniffing vulnerabilities by preventing browsers from guessing the content type
-     * sets the X-XSS-Protection header to enable the browser's cross-site scripting (XSS) filter
-     * This will control the amount of information sent in the "Referer" header, which can help protect against some types of attacks.
-     *
-     */
+    // Apply Helmet for security (e.g., XSS prevention, frameguard, HSTS)
     app.use(
         helmet({
             contentSecurityPolicy: {
@@ -127,37 +130,39 @@ async function bootstrap() {
         })
     );
 
-    //Initiating Server
+    // Start the server and listen on the configured port
     const port = configService.get('server.port');
     const server = await app.listen(port);
     appLogger.log(BaseMessage.Success.ServerStartUp + port);
+
+    // Return the server and related instances
     return { server, app, appLogger, apiBaseURL, routePrefix };
 }
 
 if (cluster.default.isPrimary && process.env.APP_ENV !== EnvironmentEnum.LOCAL) {
-    // Get the number of CPUs, but allow for a configurable WORKER_COUNT
+    // Master process: create worker processes based on CPU count
     const numCPUs = os.cpus().length;
     const workerCount = process.env.WORKER_COUNT ? parseInt(process.env.WORKER_COUNT, 10) : numCPUs;
 
-    // Set a higher limit globally for all EventEmitters
+    // Set the maximum number of listeners for events
     events.EventEmitter.defaultMaxListeners = numCPUs === 1 ? 2 : numCPUs;
 
     console.log(
         `Master process is running with PID ${process.pid}. Forking ${workerCount} workers...`
     );
 
-    // Fork workers equal to WORKER_COUNT or number of CPUs
+    // Fork worker processes for each CPU core
     for (let i = 0; i < workerCount; i++) {
         cluster.default.fork();
     }
 
-    // If a worker dies, log the event and fork a new worker
+    // Restart a worker if it dies unexpectedly
     cluster.default.on('exit', (worker) => {
         console.log(`Worker ${worker.process.pid} died. Starting a new worker...`);
         cluster.default.fork();
     });
 } else {
-    // Each worker runs its own instance of the NestJS app
+    // Worker process: start the NestJS app instance
     bootstrap()
         .then(({ server, app, appLogger, apiBaseURL, routePrefix }) => {
             gracefulServerShutdown(server, app, cluster.default.worker);
@@ -168,31 +173,38 @@ if (cluster.default.isPrimary && process.env.APP_ENV !== EnvironmentEnum.LOCAL) 
         });
 }
 
+// Gracefully handle server shutdown on receiving termination signals
 function gracefulServerShutdown(
     server: Server,
     app: NestExpressApplication,
     worker: cluster.Worker | undefined
 ) {
     let activeConnections = 0;
+
+    // Track active connections to allow graceful shutdown
     server.on('connection', (connection) => {
         activeConnections++;
         connection.on('close', () => activeConnections--);
     });
 
+    // Handle worker exit events
     worker?.on('exit', (code, signal) => {
         console.log(`Worker ${worker.process.pid} exited with code ${code} and signal ${signal}`);
         process.exit(code);
     });
 
+    // Shutdown logic to handle different termination signals
     const shutdown = async (signal: string) => {
         console.log(`Received shutdown signal (${signal}). Closing server...`);
         try {
+            // Set a timeout to force shutdown if it takes too long
             const timeout = setTimeout(async () => {
                 console.log('Forcing shutdown due to timeout.');
                 worker?.kill();
                 process.exit(1);
             }, 20000);
 
+            // Close the server and wait for existing connections to finish
             await new Promise<void>((resolve, reject) => {
                 server.close((error) => {
                     clearTimeout(timeout);
@@ -204,11 +216,13 @@ function gracefulServerShutdown(
                 });
             });
 
+            // Wait until all active connections are closed
             while (activeConnections > 0) {
                 console.log(`Waiting for ${activeConnections} connections to finish...`);
                 await new Promise((resolve) => setTimeout(resolve, 1000));
             }
 
+            // Close the NestJS application instance
             await app.close();
             worker?.disconnect();
             console.log('Application shut down gracefully.');
@@ -219,6 +233,7 @@ function gracefulServerShutdown(
         }
     };
 
+    // Attach signal handlers for SIGINT, SIGTERM, and unhandled exceptions/rejections
     process.on('SIGINT', () => shutdown('SIGINT'));
     process.on('SIGTERM', () => shutdown('SIGTERM'));
     process.on('uncaughtException', (error) => {
